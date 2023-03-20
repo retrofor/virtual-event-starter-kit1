cb360d5f553980e88603e672ce5ca22c94bd443b
简律纯 <Hsiangnianian@outlook.com>
2023-03-20 18:26:23 +0800

    🎹👀

--- pages/expo.tsx
+++ pages/expo.tsx
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { GetStaticProps } from 'next';
 
 import Page from '@components/page';
